9e1cfebee84aeed6b210b9fb81c68bdbdb3a0dbf
Imre Bartis <imbartis@gmail.com>
2024-12-11 03:30:13 +0200

    tehtävä 13.5.

---
+++ controllers/blogs.js
@@ -0,0 +1,63 @@
+const router = require('express').Router()
+
+const Blog = require('../models/blog')
+
+const blogFinder = async (req, res, next) => {
+    req.blog = await Blog.findByPk(req.params.id)
+    next()
+}
+
+router.get('/', async (req, res) => {
+  const blogs = await Blog.findAll()
+  console.log(JSON.stringify(blogs, null, 2))
+  res.json(blogs)
+})
+
+router.post('/', async (req, res) => {
+  try {
+    const blog = await Blog.create(req.body)
+    return res.json(blog)
+  } catch (error) {
+    return res.status(400).json({ error })
+  }
+})
+
+router.get('/:id', blogFinder, async (req, res) => {
+  try {
+    const id = parseInt(req.params.id, 10)
+    if (isNaN(id)) {
+      return res.status(400).json({ error: 'Invalid ID' })
+    }
+    if (!req.blog) {
+      return res.status(404).json({ error: 'Blog not found' })
+    }
+    console.log(req.blog.toJSON())
+    res.json(req.blog)
+  } catch (error) {
+    res.status(500).json({ error: error.message })
+  }
+})
+
+router.delete('/:id', blogFinder, async (req, res) => {
+  if (req.blog) {
+    await req.blog.destroy();
+    res.status(204).end();
+  } else {
+    res.status(404).json({ error: 'Blog not found' });
+  }
+});
+
+router.put('/:id', blogFinder, async (req, res) => {
+  if ( req.blog) {
+    req.blog.title = req.body.title
+    req.blog.author = req.body.author
+    req.blog.url = req.body.url
+    req.blog.likes = req.body.likes
+    await req.blog.save()
+    res.json( req.blog)
+  } else {
+    res.status(404).end()
+  }
+})
+
+module.exports = router
--- index.js
+++ index.js
@@ -1,98 +1,20 @@
-require('dotenv').config()
-
-const { Sequelize, Model, DataTypes } = require('sequelize')
 const express = require('express')
 const app = express()
+
+const { PORT } = require('./util/config')
+const { connectToDatabase } = require('./util/db')
+
+const blogsRouter = require('./controllers/blogs')
+
 app.use(express.json())
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-  dialectOptions: {
-    ssl: false
-  }
-})
+app.use('/api/blogs', blogsRouter)
 
-class Blog extends Model {}
+const start = async () => {
+  await connectToDatabase()
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+  })
+}
 
-Blog.init(
-  {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    author: {
-      type: DataTypes.STRING
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    title: {
-      type: DataTypes.STRING,
-      allowNull: false
-    },
-    likes: {
-      type: DataTypes.INTEGER
-    }
-  },
-  {
-    sequelize,
-    underscored: true,
-    timestamps: false,
-    modelName: 'blog'
-  }
-)
-
-Blog.sync()
-
-app.get('/api/blogs', async (req, res) => {
-  const blogs = await Blog.findAll()
-  console.log(JSON.stringify(blogs, null, 2))
-  res.json(blogs)
-})
-
-app.post('/api/blogs', async (req, res) => {
-  try {
-  const blog = await Blog.create(req.body)
-  return res.json(blog)
-  } catch(error) {
-    return res.status(400).json({ error })
-  }
-})
-
-app.get('/api/blogs/:id', async (req, res) => {
-  try {
-    const id = parseInt(req.params.id, 10);
-    if (isNaN(id)) {
-      return res.status(400).json({ error: 'Invalid ID' });
-    }
-    const blog = await Blog.findByPk(id);
-    if (!blog) {
-      return res.status(404).json({ error: 'Blog not found' });
-    }
-    console.log(blog.toJSON());
-    res.json(blog);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.put('/api/blogs/:id', async (req, res) => {
-  const blog = await Blog.findByPk(req.params.id)
-  if (blog) {
-    blog.title = req.body.title
-    blog.author = req.body.author
-    blog.url = req.body.url
-    blog.likes = req.body.likes
-    await blog.save()
-    res.json(blog)
-  } else {
-    res.status(404).end()
-  }
-})
-
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+start()
